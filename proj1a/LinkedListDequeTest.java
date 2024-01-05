e76a51bbd895395a3dde4a8bd28304522550358e
Alex <alexanderg2000@gmail.com>
2024-01-05 15:57:06 -0500

    redo

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,7 +1,7 @@
 /** Performs some basic linked list tests. */
 public class LinkedListDequeTest {
 	
-	/* Utility method for printing out empty checks. */
+	/** Utility method for printing out empty checks. */
 	public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
 			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
@@ -10,7 +10,7 @@ public class LinkedListDequeTest {
 		return true;
 	}
 
-	/* Utility method for printing out empty checks. */
+	/** Utility method for printing out empty checks. */
 	public static boolean checkSize(int expected, int actual) {
 		if (expected != actual) {
 			System.out.println("size() returned " + actual + ", but expected: " + expected);
@@ -19,7 +19,7 @@ public class LinkedListDequeTest {
 		return true;
 	}
 
-	/* Prints a nice message based on whether a test passed. 
+	/** Prints a nice message based on whether a test passed.
 	 * The \n means newline. */
 	public static void printTestStatus(boolean passed) {
 		if (passed) {
